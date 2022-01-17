86e6b4e2b71a7caa9db7525417c83427167901bd
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2022-01-17 23:46:20 +0800

    Add AuthController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,76 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/login', function () {
+    return view('dashboard/auth/login');
+});
+
+Route::post('/login', [AuthController::class,'authenticate']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard/dashboard');
+    });
+
+    Route::get('/beranda', function () {
+        return view('dashboard/layout/beranda');
+    });
+
+    Route::get('/sorotan', function () {
+        return view('dashboard/layout/sorotan');
+    });
+
+    Route::get('/product', function () {
+        return view('dashboard/layout/product');
+    });
+
+    Route::get('/galery', function () {
+        return view('dashboard/layout/galery');
+    });
+
+    Route::get('/testimoni', function () {
+        return view('dashboard/layout/testimoni');
+    });
+
+    Route::get('/kontak', function () {
+        return view('dashboard/layout/kontak');
+    });
+
+    Route::get('/admin', function () {
+        return view('dashboard/layout/admin');
+    });
+
+    // Form Routes
+
+    Route::get('/edit-beranda', function () {
+        return view('dashboard/form/beranda');
+    });
+
+    Route::get('/edit-sorotan', function () {
+        return view('dashboard/form/sorotan');
+    });
+
+    Route::get('/edit-product', function () {
+        return view('dashboard/form/product');
+    });
+
+    Route::get('/edit-galery', function () {
+        return view('dashboard/form/galery');
+    });
+
+    Route::get('/edit-testimoni', function () {
+        return view('dashboard/form/testimoni');
+    });
+
+    Route::get('/edit-kontak', function () {
+        return view('dashboard/form/kontak');
+    });
+
+    Route::get('/edit-admin', function () {
+        return view('dashboard/form/admin');
+    });
+});
+
+
+
