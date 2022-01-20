a63c64a145477e5ab691b9a62a22b1e60be52b11
Restu Indrawan <57904667+restuindrawan@users.noreply.github.com>
2022-01-20 23:16:03 +0800

    Update Dashboard Credit

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2021 &copy; Mazer</p>
+            <p>2022 &copy; Karya Alam Abadi</p>
         </div>
         <div class="float-end">
-            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                    href="http://ahmadsaugi.com">A. Saugi</a></p>
+            <p><span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                    href="https://se.ittelkom-pwt.ac.id/">Software Engineering 19</a></p>
         </div>
     </div>
 </footer>
