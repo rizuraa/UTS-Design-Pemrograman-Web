46899f86e2badefecbf4cbbf75ef47ec6e5f7cd1
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2022-01-21 19:47:04 +0700

    duh

--- karya_alam_abadi (1).sql
+++ karya_alam_abadi (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2022 at 05:28 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Generation Time: Jan 21, 2022 at 01:46 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `karya_alam_abadi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `abouts`
+--
+
+CREATE TABLE `abouts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `judul` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `abouts`
+--
+
+INSERT INTO `abouts` (`id`, `judul`, `deskripsi`, `created_at`, `updated_at`) VALUES
+(2, 'Test 3', 'lorem ipsum keun test', '2022-01-19 11:10:43', '2022-01-20 01:20:01'),
+(3, 'Test 4', 'Lorem push', '2022-01-19 21:28:31', '2022-01-20 01:18:32');
 
 -- --------------------------------------------------------
 
@@ -67,7 +89,55 @@ CREATE TABLE `galeries` (
 --
 
 INSERT INTO `galeries` (`id`, `gambar`, `created_at`, `updated_at`) VALUES
-(1, 'Io9dl0wYfuVi57yv1jnBThRRAONF10qPG7YOH9eN.JPG', '2022-01-17 08:13:49', '2022-01-17 08:13:49');
+(1, 'AaPAwWMNmXMg2xMYzUlMgQbemdUuUp3CEBNqt55P.jpg', '2022-01-17 23:18:28', '2022-01-17 23:18:28'),
+(2, 'Rjm0NP90eU6qH8YoqyKS0rRGLbj3aXTZjSK5KtaX.PNG', '2022-01-17 23:18:48', '2022-01-17 23:18:48'),
+(3, 'lX6YwoJ7CNp6zwnBfKDwojMxFIMK48bGmvvf8Oiz.PNG', '2022-01-17 23:18:56', '2022-01-17 23:18:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `homes`
+--
+
+CREATE TABLE `homes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `homes`
+--
+
+INSERT INTO `homes` (`id`, `nama`, `deskripsi`, `gambar`, `created_at`, `updated_at`) VALUES
+(3, 'CV Karya Mulya Abadi', 'lorem ipsum keun Amet', 'I69kPH0AwZMtQIowgan691gdXg9MeZwSsZzpEPBB.PNG', '2022-01-21 05:30:15', '2022-01-21 05:30:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kontaks`
+--
+
+CREATE TABLE `kontaks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `social` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `maps` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `kontaks`
+--
+
+INSERT INTO `kontaks` (`id`, `alamat`, `email`, `telp`, `social`, `maps`, `created_at`, `updated_at`) VALUES
+(3, 'Purwokerto', 'mfmaulana08@gmail.com', '081024780821', 'IG CV karya alam', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.1234446337803!2d109.20283051536238!3d-7.561517376771922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e654289ed28151b%3A0x5c46585f8b13b485!2sCV.KARYA%20ALAM%20ABADI!5e0!3m2!1sen!2sid!4v1637888653713!5m2!1sen!2sid', '2022-01-17 23:21:53', '2022-01-17 23:21:53');
 
 -- --------------------------------------------------------
 
@@ -86,12 +156,17 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2022_01_10_124453_create_admins_table', 1),
-(6, '2022_01_17_155028_create_galeries_table', 2);
+(8, '2022_01_18_014116_create_homes_table', 4),
+(10, '2014_10_12_000000_create_users_table', 5),
+(11, '2014_10_12_100000_create_password_resets_table', 5),
+(12, '2019_08_19_000000_create_failed_jobs_table', 5),
+(13, '2019_12_14_000001_create_personal_access_tokens_table', 5),
+(14, '2022_01_10_124453_create_admins_table', 5),
+(15, '2022_01_17_155028_create_galeries_table', 5),
+(16, '2022_01_18_001823_create_kontaks_table', 5),
+(17, '2022_01_18_015344_create_homes_table', 5),
+(18, '2022_01_19_160527_create_abouts_table', 6),
+(19, '2022_01_20_132203_create_produks_table', 7);
 
 -- --------------------------------------------------------
 
@@ -126,6 +201,28 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `produks`
+--
+
+CREATE TABLE `produks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `judul` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `produks`
+--
+
+INSERT INTO `produks` (`id`, `judul`, `deskripsi`, `gambar`, `created_at`, `updated_at`) VALUES
+(2, 'Test', 'asdafafs', 'P80e2NSs3aWWlhnyLDU30QxdmGixpYxGZTcEP8xO.PNG', '2022-01-20 12:51:55', '2022-01-20 12:51:55');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -152,6 +249,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 --
 
 --
+-- Indexes for table `abouts`
+--
+ALTER TABLE `abouts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `admins`
 --
 ALTER TABLE `admins`
@@ -168,6 +271,18 @@ ALTER TABLE `failed_jobs`
 -- Indexes for table `galeries`
 --
 ALTER TABLE `galeries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `homes`
+--
+ALTER TABLE `homes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kontaks`
+--
+ALTER TABLE `kontaks`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -191,6 +306,12 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `produks`
+--
+ALTER TABLE `produks`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -200,6 +321,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `abouts`
+--
+ALTER TABLE `abouts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `admins`
@@ -217,19 +344,37 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `galeries`
 --
 ALTER TABLE `galeries`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `homes`
+--
+ALTER TABLE `homes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kontaks`
+--
+ALTER TABLE `kontaks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produks`
+--
+ALTER TABLE `produks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
